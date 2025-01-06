4311c7fbbce5c28ee15a817922712507d68243df
claudio duchi <claduc@gmail.com>
2025-01-06 22:17:58 +0100

    nuove prove

--- Myunit/mytype.pas
+++ Myunit/mytype.pas
@@ -10,24 +10,23 @@ type
   TTableData = array of array of string;
   TBooleanArray = array of Boolean;
   // gestione allineamento tabella
-   TTabella = record
-    colonne: integer;
-    caption: string;
-    allineamenti: string;
-    intestazione: string;
-  end;
-   // \usepackage[left=30mm,right=15mm,top=20mm,bottom=20mm,bindingoffset=0cm,showframe,]{geometry}
-   type
-  TTDimPagina = record
-    left: string;
-    right: string;
-    top: string;
-    bottom: string;
-    bindingoffset: string;
-    showframe: boolean;
-  end;
+    TTabella = record
+      colonne: integer;
+      caption: string;
+      allineamenti: string;
+      intestazione: string;
+    end;
+  // \usepackage[left=30mm,right=15mm,top=20mm,bottom=20mm,bindingoffset=0cm,showframe,]{geometry}
+    TTDimPagina = record
+      left: string;
+      right: string;
+      top: string;
+      bottom: string;
+      bindingoffset: string;
+      showframe: boolean;
+    end;
 
-implementation
+  implementation
 
-end.
+  end.
 
